302dd30d34994b7efe4995435b67c881fc3a1b4d
Marcos Galvão <mgalvao@ciandt.com>
2022-07-08 11:48:24 -0300

    task 0 Spring Boot API REST

---
+++ 0x02/construindo_API_REST/SongApi/src/main/java/com/example/demo/SongRepository.java
@@ -0,0 +1,48 @@
+package com.example.demo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class SongRepository {
+    private List<Song> list = new ArrayList<Song>();
+
+    public List<Song> getAllSongs() {
+        return list;
+    }
+
+    public Song getSongById(Integer id) {
+        for (Song song : list) {
+            if (song.getId().equals(id)) {
+                return song;
+            }
+        }
+        return null;
+    }
+
+    public void addSong(Song s) {
+        this.list.add(s);
+    }
+
+    public void updateSong(Song s) {
+        for (int i=0; i < this.list.size(); i++) {
+            if (list.get(i).getId().equals(s.getId())) {
+                list.remove(i);
+                list.add(s);
+                break;
+            }
+        }
+    }
+
+    public void removeSong(Song s) {
+        for (int i=0; i < this.list.size(); i++) {
+            if (list.get(i).getId().equals(s.getId())) {
+                list.remove(i);
+                break;
+            }
+        }
+    }
+    
+}
