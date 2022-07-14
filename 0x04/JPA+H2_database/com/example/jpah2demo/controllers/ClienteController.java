296f968fc7e2a23a2cd3c65239359efda6224382
Marcos Galvão <mgalvao@ciandt.com>
2022-07-13 23:05:40 -0300

    task 0 Spring Data and H2

---
+++ 0x04/JPA+H2_database/com/example/jpah2demo/controllers/ClienteController.java
@@ -0,0 +1,62 @@
+package com.example.jpah2demo.controllers;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+
+import com.example.jpah2demo.models.Cliente;
+import com.example.jpah2demo.repositories.ClienteRepository;
+
+@RestController
+public class ClienteController {
+    
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+    @PostMapping("/addClient")
+    public ResponseEntity<Cliente> addClient(@RequestBody Cliente cliente) {
+        Cliente savedCliente = clienteRepository.save(cliente);
+        return generateOKResponseEntity(savedCliente);
+    }
+
+    @GetMapping("/findAllClients")
+    public ResponseEntity<List<Cliente>> findAllClients() {
+        List<Cliente> clientes = clienteRepository.findAll();
+        return generateOKResponseEntity(clientes);
+    }
+
+    @GetMapping("/findClientById/{id}")
+    public ResponseEntity<Cliente> findClientById(@PathVariable("id") Long idClient) {
+        Cliente cliente = clienteRepository.findById(idClient).get();
+        return generateOKResponseEntity(cliente);
+    }
+
+    @DeleteMapping("/removeClientById/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void removerCliente(@PathVariable("id") Long idClient){
+        clienteRepository.deleteById(idClient);
+    }
+
+    @PutMapping("/updateClientById/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void updateCliente(@PathVariable("id") Long id, @RequestBody Cliente cliente){
+        cliente.setId(id);
+        clienteRepository.save(cliente);
+    }
+
+    private <T> ResponseEntity<T> generateOKResponseEntity(T content) {
+        return new ResponseEntity<>(content, HttpStatus.OK);
+    }
+}
---
+++ 0x04/JPA+H2_database/jpa-h2-demo/src/main/java/com/example/jpah2demo/models/Cliente.java
@@ -0,0 +1,66 @@
+package com.example.jpah2demo.models;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+
+@Entity
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+public class Cliente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+    private int idade;
+    private String email;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    @ToString.Exclude
+    private List<Telefone> telefones;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    @ToString.Exclude
+    private List<Endereco> enderecos;
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Cliente other = (Cliente) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+
+    
+}
