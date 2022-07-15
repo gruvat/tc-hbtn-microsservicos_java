040f9c6bf6b1f634e93bb710396136fa51beddcc
Marcos Galvão <mgalvao@ciandt.com>
2022-07-15 12:11:10 -0300

    Spring reccomended way to intercept requests

--- 0x05/MegaSenaAPI/MegaSenaAPI/src/main/java/com/song/MegaSenaAPI/ApplicationSecurityConfig.java
+++ 0x05/MegaSenaAPI/MegaSenaAPI/src/main/java/com/song/MegaSenaAPI/ApplicationSecurityConfig.java
@@ -1,18 +1,20 @@
 package com.song.MegaSenaAPI;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableWebSecurity
-public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+//@EnableWebSecurity
+public class ApplicationSecurityConfig {
 
-    @Override
-    public void configure(HttpSecurity http) throws Exception {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests()
             .anyRequest().permitAll();
+        
+        return http.build();
     }
-    
+
 }
