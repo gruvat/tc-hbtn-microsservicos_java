7e572a2e5e5658f48d6b170fb54f49e35129cfe7
Marcos Galvão <mgalvao@ciandt.com>
2022-07-16 19:35:12 -0300

    adjusts to pass checker

--- 0x07/api-rest-calculator/calculator/src/test/java/com/example/calculator/controller/CalculatorControllerTest.java
+++ 0x07/api-rest-calculator/calculator/src/test/java/com/example/calculator/controller/CalculatorControllerTest.java
@@ -1,8 +1,10 @@
 package com.example.calculator.controller;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
@@ -12,6 +14,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import java.time.LocalDate;
 
+@ExtendWith(SpringExtension.class)
 @WebMvcTest(CalculatorController.class)
 public class CalculatorControllerTest {
     @Autowired
